3aa9a2602be25547c0907cb414638a046cb1df3d
Mamagase <gn.mamagase@gmail.com>
2021-08-03 09:12:56 +0200

    i will be working on login and sigin functions

---
+++ application/controllers/Auth.php
@@ -0,0 +1,23 @@
+ <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller 
+{
+
+	public function index()
+	{	
+		$data['title'] = 'User Login';
+		$this->load->view('templates/auth_header', $data);
+		$this->load->view('auth/login');
+		$this->load->view('templates/auth_footer');
+	}
+
+	public function registration()
+	{
+		$data['title'] = 'User Registration';
+		$this->load->view('templates/auth_header', $data);
+		$this->load->view('auth/registration');
+		$this->load->view('templates/auth_footer');
+	}
+
+}
