12ebd744f95bd20f7ec18c41a23304312a2c2ee3
Mamagase <gn.mamagase@gmail.com>
2021-08-03 22:57:28 +0200

    i completed register and login functions

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,21 +3,110 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller 
 {
-
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
 	public function index()
 	{	
-		$data['title'] = 'User Login';
-		$this->load->view('templates/auth_header', $data);
-		$this->load->view('auth/login');
-		$this->load->view('templates/auth_footer');
+		$this->form_validation->set_rules('email', 'Email','trim|required|valid_email');
+		$this->form_validation->set_rules('password','Password','trim|required');
+
+		if ($this->form_validation->run() == false) {
+			$data['title'] = 'User Login';
+			$this->load->view('templates/auth_header', $data);
+			$this->load->view('auth/login');
+			$this->load->view('templates/auth_footer');
+		}
+		else
+		{
+			//success validation
+			$this->_login();
+		}
+	}
+
+	private function _login()
+	{
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
+
+		$user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+		//check user exists
+		if ($user) {
+			//check user active
+			if ($user['is_active'] == 1) {
+				//check password
+				if (password_verify($password, $user['password'])) {
+					$data = [
+						'email' =>  $user['email'],
+						'role_id' => $user['role_id']
+					];
+
+					$this->session->set_userdata($data);
+					redirect('user');
+				}
+				else
+				{
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong Password!</div>');
+					redirect('auth');
+				}
+			}
+			else
+			{
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not been activated!</div>');
+				redirect('auth');
+			}
+		}
+		else
+		{
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered! Please create an account.</div>');
+			redirect('auth');
+		}
 	}
 
 	public function registration()
 	{
-		$data['title'] = 'User Registration';
-		$this->load->view('templates/auth_header', $data);
-		$this->load->view('auth/registration');
-		$this->load->view('templates/auth_footer');
+		//setting roles for inputs
+		
+		$this->form_validation->set_rules('name','Name','required|trim');
+		$this->form_validation->set_rules('email','Email','required|trim|valid_email|is_unique[user.email]', ['is_unique' => 'Email already exists!']);
+		$this->form_validation->set_rules('password1','Password','required|trim|min_length[3]|matches[password2]', ['matches' => 'password dont match!', 'min_length' => 'Password too short!']);
+		$this->form_validation->set_rules('password2','Password','required|trim|min_length[3]|matches[password1]');
+
+		//check form validation
+		if ($this->form_validation->run() == false) {
+			$data['title'] = 'User Registration';
+			$this->load->view('templates/auth_header', $data);
+			$this->load->view('auth/registration');
+			$this->load->view('templates/auth_footer');
+		}
+		else
+		{
+			$data = [
+				'name' => htmlspecialchars($this->input->post('name', true)), 
+				'email' => htmlspecialchars($this->input->post('email', true)),
+				'image' => 'default.jpg',
+				'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+				'role_id' => 2,
+				'is_active' => 1,
+				'date_created' => date('Y-m-d')
+			];
+
+			$this->db->insert('user', $data);
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Your Acounnt has been created successfully! Please Login</div>');
+			redirect('auth');
+		}
 	}
 
-}
+
+	public function logout()
+	{
+		$this->session->unset_userdata('email');
+		$this->session->unset_userdata('role_id');
+
+		$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You have been logged out!</div>');
+		redirect('auth');
+	}
+} 
