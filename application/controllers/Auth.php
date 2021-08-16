83961db6cf627e0eea32fd1efc1718885f885f2e
Mamagase <gn.mamagase@gmail.com>
2021-08-16 19:44:31 +0200

    i've been working on user activation

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -102,22 +102,115 @@ class Auth extends CI_Controller
 		}
 		else
 		{
+			$email = $this->input->post('email', true);
 			$data = [
 				'name' => htmlspecialchars($this->input->post('name', true)), 
-				'email' => htmlspecialchars($this->input->post('email', true)),
+				'email' => htmlspecialchars($email),
 				'image' => 'default.jpg',
 				'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
 				'role_id' => 2,
-				'is_active' => 1,
+				'is_active' => 0,
+				'date_created' => time()
+			];
+
+			//token
+			$token = base64_encode(random_bytes(32));
+			$user_token = [
+				'email' => $email,
+				'token' => $token,
 				'date_created' => time()
 			];
 
 			$this->db->insert('user', $data);
-			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Your Acounnt has been created successfully! Please Login</div>');
+			$this->db->insert('user_token', $user_token);
+
+			$this->_sendEmail($token, 'verify');
+
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Please check your email to activate your account!</div>');
 			redirect('auth');
 		}
 	}
 
+
+	private function _sendEmail($token, $type)
+	{
+		$config = [
+			'protocol'  => 'smtp',
+			'smtp_host' => 'ssl://smtp.googlemail.com',
+			'smtp_user' => 'gn.mamagase@mail.com',
+			'smtp_pass' => '19980202@m',
+			'smtp_port' => 25,
+			'mailtype'  => 'html',
+			'charset'   => 'utf-8',
+			'newline'   => '\r\n'
+		];
+
+		$this->load->library('email',$config);
+
+		$this->email->from('gn.mamagase@gmail.com', 'Role Management System');
+		$this->email->to($this->input->post('email'));
+
+		if ($type == 'verify') 
+		{
+			$this->email->subject('Account Verification');
+			$this->email->message('Click this link to verify your account : ' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '&token=' . $token );
+		}
+
+		if($this->email->send())
+		{
+			return true;
+		}
+		else
+		{
+			echo $this->email->print_debugger();
+			die;
+		}
+	}
+
+	public function verify()
+	{
+		$email = $this->input->get('email');
+		$token = $this->input->get('token');
+
+		$user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+		if ($user) 
+		{
+			$user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+			if ($user_token) 
+			{
+				if (time() - $user_token['date_created'] < (60 * 60 * 24)) 
+				{
+					$this->db->set('is_active', 1);
+					$this->db->where('email', $email);
+					$this->db->update('user');
+
+					$this->db->delete('user_token', ['email' => $email]);
+
+					$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">' . $email . ' has been activated! Please login.</div>');
+					redirect('auth');	
+				}
+				else
+				{
+					$this->db->delete('user', ['email' => $email]);
+					$this->db->delete('user_token', ['email' => $email]);
+
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Token exprired.</div>');
+					redirect('auth');				
+				}
+			}
+			else
+			{
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Wrong token.</div>');
+				redirect('auth');
+			}
+		}
+		else
+		{
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Wrong email.</div>');
+			redirect('auth');	
+		}
+	}
 
 	public function logout()
 	{
